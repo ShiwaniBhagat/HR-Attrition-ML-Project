1d82daa020275fd1a8490c8ee024930d9c7c31e0
ShiwaniBhagat <152151763+ShiwaniBhagat@users.noreply.github.com>
2025-12-12 18:16:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 import streamlit as st
 import numpy as np
 import pickle
+# Load Model & Scaler
+
+model = pickle.load(open("best_model.pkl", "rb"))
+scaler = pickle.load(open("scaler.pkl", "rb"))
+
 
 
