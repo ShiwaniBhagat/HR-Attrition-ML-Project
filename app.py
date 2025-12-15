9b0fa1712728252fd5584b37d1d774232f679646
ShiwaniBhagat <152151763+ShiwaniBhagat@users.noreply.github.com>
2025-12-15 18:13:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,13 @@ import pickle
 model = pickle.load(open("best_model.pkl", "rb"))
 scaler = pickle.load(open("scaler.pkl", "rb"))
 
+# Streamlit Page Config
+
+st.set_page_config(
+    page_title="HR Attrition Prediction",
+    page_icon="💼",
+    layout="centered"
+)
+
 
 
